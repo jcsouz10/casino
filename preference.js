19f5699907f895dc7dd51bda478471566a488143
Jean <jeanrt@hotmail.com>
2022-09-29 20:48:46 -0300

    PROD MERCADO

--- preference.js
+++ preference.js
@@ -1,15 +1,15 @@
 const mercadopago = require ('mercadopago');
 // Add Your credentials
 mercadopago.configure({
-  access_token: 'TEST-415585518666585-050622-932ce1a48342385a47c09514c5761af8-119077743'
+  access_token: 'APP_USR-415585518666585-050622-c17f7d6c3717b37130dca9668548c51e-119077743'
 });
 
 let preference = {
     items: [
       {
-        title: 'My Item',
+        title: 'PDF',
         unit_price: 100,
-        quantity: 1,
+        quantity: 9999,
       }
     ]
   };
